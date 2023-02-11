cdf5b9610ce75510c5bc576b7e51153018a6c1da
DataBook4U (Sebastian) <opocensky.sebastian@outlook.de>
2023-02-11 18:17:49 +0100

    adjusted input nft generes

--- main.py
+++ main.py
@@ -138,39 +138,65 @@ class TokenTransferEvents:
 #Dictionary for the ContractAdresses of the NFT Collections we are getting the Data from
 #After "The Sandbox" the Top 10 Art NFTs where included
 Contract_Adresses = {
-    "Bored Ape Yacht Club": "0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d",
-    "CryptoPunks": "0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb",
-    "Mutant Ape Yacht Club": "0x60e4d786628fea6478f785a6d7e704777c86a7c6",
-    "Otherdeed for Otherside": "0x34d85c9cdeb23fa97cb08333b511ac86e1c4e258",
-    "Azuki": "0xed5af388653567af2f388e6224dc7c4b3241c544",
-    "Clone X - X Takashi": "0x49cf6f5d44e70224e2e23fdcdd2c053f30ada28b",
-    "Moonbirds": "0x23581767a106ae21c074b2276d25e5c3e136a68b",
-    "Doodles": "0x8a90cab2b38dba80c64b7734e58ee1db38b8992e",
-    "Bored Ape Kennel Club": "0xba30e5f9bb24caa003e9f2f0497ad287fdf95623",
-    "The Sandbox": "0x5cc5b05a8a13e3fbdb0bb9fccd98d38e50f90c38",
-    "CrypToadz": "0x1cb1a5e65610aeff2551a50f76a87a7d3fb649c6",
+    "Bored Ape Yacht Club_art": "0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d",
+    "CryptoPunks_art": "0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb",
+    "Rarible": "0xd07dc4262bcdbf85190c01c996b4c06a461d2430",
     "SuperRare": "0xb932a70a57673d89f4acffbe830e8ed7f75fb9e0",
-    "Chromie Squiggle": "0x059edd72cd353df5106d2b9cc5ab83a52287ac3a",
-    "My Curio Cards": "0x73da73ef3a6982109c4d5bdb0db9dd3e3783f313",
-    "Deadfellaz": "0x2acab3dea77832c09420663b0e1cb386031ba17b",
-    "Meridian": "0xa7d8d9ef8d8ce8992df33d8b8cf4aebabd5bd270",
-    "Checks": "0x34eebee6942d8def3c125458d1a86e0a897fd6f9",
-    "Dooplicator": "0x466cfcd0525189b573e794f554b8a751279213ac",
-    "FULL SEND METACARD NFT": "0x7ecb204fed7e386386cab46a1fcb823ec5067ad5",
-    "MakersPlace": "0x2a46f2ffd99e19a89476e2f62270e0a35bbf0756",
+    "Karafuru": "0xd2f668a8461d6761115daf8aeb3cdf5f40c532c6",
+    "Hashmasks": "0xc2c747e0f7004f9e8817db2ca4997657a7746928",
+    "My Curio Cards_art": "0x73da73ef3a6982109c4d5bdb0db9dd3e3783f313",
+    "3Landers": "0xb4d06d46a8285f4ec79fd294f78a881799d8ced9",
+    "PhantaBear": "0x67d9417c9c3c250f61a83c7e8658dac487b56b09",
+    "Creepz by OVERLORD": "0xfe8c6d19365453d26af321d0e8c910428c23873f",
+
+    "CryptoPunks_coll": "0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb",
+    "Bored Ape Yacht Club_coll": "0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d",
+    "Mutant Ape Yacht Club": "0x60e4d786628fea6478f785a6d7e704777c86a7c6",
+    "Azuki": "0xed5af388653567af2f388e6224dc7c4b3241c544",
+    "Clone X - X TAKASHI MURAKAMI": "0x49cf6f5d44e70224e2e23fdcdd2c053f30ada28b",
+
+    "Ethereum Name Service": "0x57f1887a8bf19b14fc0df6fd9b2acc9af147ea85",
+    "Decentraland Names": "0x2a187453064356c898cae034eaed119e1663acb8",
+    "dotbit(.bit)": "0x2a187453064356c898cae034eaed119e1663acb8",
+    "Unstoppable Domains": "0xa9a6a3626993d487d2dbda3173cf58ca1a9d9e9f",
+    "Ether Name Service": "0xcc7187ddbe8f099d31bac88d8d67f793001d718e",
+
+    "WVRPS by WarpSound (Official)": "0xcbc67ea382f8a006d46eeeb7255876beb7d7f14d",
+    "The Orbs by BT": "0x52e66ca968010d064938a8099a172cbaaf08c125",
+    "EulerBeats Genesis": "0x8754f54074400ce745a7ceddc928fb1b7e985ed6",
+    "EulerBeats Enigma": "0xa98771a46dcb34b34cdad5355718f8a97c8e603e",
+    "KINGSHIP": "0x9f83b08d90eeda539f7e2797fed3f6996917bba8",
+
+    "Justin Aversano - Twin Flames": "0x495f947276749ce646f68ac8c248420045cb7b5e",
+    "Where My Vans Go": "0x509a050f573be0d5e01a73c3726e17161729558b",
+    "Editions x Guido": "0x8887ce34f6f1a4de4e8eb2a9195eeb261c413365",
+    "Women Unite": "0xbee7cb80dfd21a9eaae714208f361601f68eb746",
+    "First Day Out by DrifterShoots": "0x6913233ada65330adf01f24f715dffcc60497cc8",
+
+    "Sorare": "0x629a673a8242c2ac4b7b8c5d8735fbeac21a6205",
+    "VaynerSports Pass VSP": "0xbce6d2aa86934af4317ab8615f89e3f9430914cb",
+    "The Association": "0xd7bea2b69c7a1015aadaa134e564eee6d34149c0",
+    "F1 Delta Time": "0x2af75676692817d85121353f0d6e8e9ae6ad5576",
+    "Legion DAO": "0x7c6f282efbe06e93de4ab5e646478bee20f966b8",
+    "MLB Champions": "0x7c6f282efbe06e93de4ab5e646478bee20f966b8",
+
     "Parallel Alpha": "0x76be3b62873462d2142405439777e971754e8e77",
-    "Wolf Game": "0x76be3b62873462d2142405439777e971754e8e77",
-    "Worldwide Webb Land": "0xa1d4657e0e6507d5a94d06da93e94dc7c8c44b51",
-    "Town Star": "0xc36cf0cfcb5d905b8b513860db0cfe63f6cf9f5c",
-    "Impostors Genesis Alien": "0x3110ef5f612208724ca51f5761a69081809f03b7",
-    "Rumble Kong League": "0xef0182dc0574cd5874494a120750fd222fdb909a",
-    "My Pet Hooligan": "0x09233d553058c2f42ba751c87816a8e9fae7ef10",
-    "Mirandus": "0xc36cf0cfcb5d905b8b513860db0cfe63f6cf9f5c",
-    "Pixelmon": "0x32973908faee0bf825a343000fe412ebe56f802a",
-    "Treeverse Plots": "0x1b829b926a14634d36625e60165c0770c09d02b2",
-    "adidas Originals Into the Metaverse": "0x28472a58a490c5e09a238847f66a68a47cc76f0f",
-    "PROOF Collective": "0x08d7c0242953446436f34b4c78fe9da38c73668d",
-    "oncyber labs": "0x226bf5293692610692e2c996c9875c914d2a7f73"
+    "My Curio Cards_tradeCards": "0x73da73ef3a6982109c4d5bdb0db9dd3e3783f313",
+    "Trump Digital Trading Cards": "0x24a11e702cd90f034ea44faf1e180c0c654ac5d9",
+    "Meme Ltd.": "0xe4605d46fd0b3f8329d936a8b258d69276cba264",
+    "Playing Arts Crypto Edition": "0xc22616e971a670e72f35570337e562c3e515fbfe",
+
+    "Emblem Vault": "0x82c7a8f707110f5fbb16184a5933e9f78a34c6ab",
+    "Metroverse Genesis": "0x0e9d6552b85be180d941f1ca73ae3e318d2d4f1f",
+    "oncyber labs": "0x226bf5293692610692e2c996c9875c914d2a7f73",
+    "Project NANOPASS": "0xf54cc94f1f2f5de012b6aa51f1e7ebdc43ef5afc",
+    "JRNY Club": "0x0b4b2ba334f476c8f41bfe52a428d6891755554d",
+
+    "Otherdeed for Otherside": "0x34d85c9cdeb23fa97cb08333b511ac86e1c4e258",
+    "The Sandbox": "0x5cc5b05a8a13e3fbdb0bb9fccd98d38e50f90c38",
+    "Decentraland": "0xf87e31492faf9a91b02ee0deaad50d51d56d5d4d",
+    "NFT Worlds": "0xbd4455da5929d5639ee098abfaa3241e9ae111af",
+    "Worldwide Webb Land": "0xa1d4657e0e6507d5a94d06da93e94dc7c8c44b51"
 
 
 }
@@ -188,15 +214,25 @@ def getDF(Contract_Adresses: dict):
         dfs.append(df_A)
 
     df1 = pd.concat(dfs[:10])
-    df1["label"] = "NFT_all"
-    df2 = pd.concat(dfs[10:20])
-    df2["label"] = "NFT_art"
-    df3 = pd.concat(dfs[20:30])
-    df3["label"] = "NFT_gaming"
-    df4 = pd.concat(dfs[30:])
-    df4["label"] = "NFT_membership"
+    df1["label"] = "NFT_art"
+    df2 = pd.concat(dfs[10:15])
+    df2["label"] = "NFT_collectibles"
+    df3 = pd.concat(dfs[15:20])
+    df3["label"] = "NFT_DomainNames"
+    df4 = pd.concat(dfs[20:25])
+    df4["label"] = "NFT_music"
+    df5 = pd.concat(dfs[25:30])
+    df5["label"] = "NFT_photograpy"
+    df6 = pd.concat(dfs[30:36])
+    df6["label"] = "NFT_sports"
+    df7 = pd.concat(dfs[36:41])
+    df7["label"] = "NFT_trading_cards"
+    df8 = pd.concat(dfs[41:46])
+    df8["label"] = "NFT_utility"
+    df9 = pd.concat(dfs[46:51])
+    df9["label"] = "NFT_virtual_worlds"
 
-    df = pd.concat([df1, df2, df3])
+    df = pd.concat([df1, df2, df3, df4, df5, df6, df7, df8, df9])
     return df
 
 df = getDF(Contract_Adresses)
